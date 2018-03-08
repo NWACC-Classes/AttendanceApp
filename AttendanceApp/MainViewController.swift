d1ec86fdf14fafbb0255ac178609e0b1041098ed
Allen Baker <abaker@nwacc.edu>
2018-03-08 09:27:18 -0600

    Renamed old project and moved all files to linked folder

---
+++ AttendanceApp/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  AttendanceApop
+//
+//  Created by Baker, Allen on 3/6/18.
+//  Copyright © 2018 Baker, Allen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Main";
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AttendanceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AttendanceApop
+//
+//  Created by Baker, Allen on 2/13/18.
+//  Copyright © 2018 Baker, Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
