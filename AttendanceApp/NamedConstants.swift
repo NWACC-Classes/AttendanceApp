59d086332e740d9c0f9d5ee0aea8fa09669bebcf
Allen Baker <abaker@nwacc.edu>
2018-03-16 12:03:17 -0500

    Added NamedConstants file and tweaked MainVC'

---
+++ AttendanceApp/NamedConstants.swift
@@ -0,0 +1,18 @@
+//
+//  NamedConstants.swift
+//  AttendanceApp
+//
+//  Created by Baker, Allen on 3/16/18.
+//  Copyright © 2018 Baker, Allen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Named Colors - from selected palette
+let extraDark = UIColor(red: 0x33/255.0, green: 0x18/255.0, blue: 0x32/255.0, alpha: 1.0)
+let mediumDark = UIColor(red: 0x69/255.0, green: 0x4D/255.0, blue: 0x75/255.0, alpha: 1.0)
+let medium = UIColor(red: 0x1B/255.0, green: 0x52/255.0, blue: 0x99/255.0, alpha: 1.0)
+let mediumLight = UIColor(red: 0x9F/255.0, green: 0xC2/255.0, blue: 0xCC/255.0, alpha: 1.0)
+let extraLight = UIColor(red: 0xF1/255.0, green: 0xEC/255.0, blue: 0xCE/255.0, alpha: 1.0)
+
