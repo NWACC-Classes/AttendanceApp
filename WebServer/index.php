742a3f1bd4eb3e3c59755a63dcabf2c47d156581
MrLopes93 <36670006+MrLopes93@users.noreply.github.com>
2018-04-27 17:15:26 -0500

    Update index.php
    Version 1.01: Added the last_insert_id to the returns data for when a new row is inserted.

--- WebServer/index.php
+++ WebServer/index.php
@@ -1,19 +1,17 @@
-<?php
-
-error_reporting(E_ALL & ~E_NOTICE);
+<?php error_reporting(E_ALL & ~E_NOTICE);
 		
 class DB {
 
 	const DB_SERV = "192.168.64.2"	;
 	const DB_USER = "standard"		;
 	const DB_PASS = "4321"			;
-	const DB_NAME =	"meeting_app"	;
+	const DB_NAME =	"meeting_app_2"	;
 	
 	protected $__conn;
 
 	function __construct() {
     	// Create connection
-		$this->conn = mysqli_connect(self::DB_SERV, self::DB_USER, self::DB_PASS, self::DB_NAME);
+		$this->conn = new mysqli(self::DB_SERV, self::DB_USER, self::DB_PASS, self::DB_NAME);
 
 		// Check connection
 		if ($this->conn->connect_error) {
@@ -25,28 +23,33 @@ class DB {
     	$this->conn->close();
 	}   
     
-	function execute($sql = null){	
+	function execute($sql = null){
+	
+		if (empty($sql) ){
+			$this->result("error", "No SQL provided" );  
+		}	
 		//Execute SQL
-		$q = mysqli_query($this->conn, $sql );	
+		$q = $this->conn->query($sql);	
 		
 		if ( empty($this->conn->error) ) {		
-			if ( $q->num_rows > 0 || 1==1){
+			if ( $q->num_rows > 0 ){
 				 $q->data_seek(0);
 				 while( $row = $q->fetch_assoc()) $data[] = $row;
+			} else {
+				$data['insert_id'] = $this->conn->insert_id;
 			}
-    		$this->result("success", "transaction executed", mysqli_num_rows($q));
+    		$this->result("success", "transaction executed", $data);
 		} else {
 			$this->result("error",    $this->conn->error  );   	
 		}
 	}
 	
 	function result($status, $msg =null, $data = null) {
-	
-									$json[ 'status']= $status;
+						$json[ 'status']= $status;
 		if ( ! empty( $msg ) 	)	$json[ 'msg' ]	= $msg;
 		if ( ! empty( $data ) 	)	$json[ 'data' ]	= $data;
 
-		$encoded	= json_encode( $json, JSON_UNESCAPED_SLASHES, JSON_PRETTY_PRINT );
+		$encoded	= json_encode( $json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT );
 		$etag		= md5( $encoded ); 
 
 		if ( isset( $_SERVER[ 'HTTP_IF_NONE_MATCH' ] ) && trim( $_SERVER[ 'HTTP_IF_NONE_MATCH' ] ) == $etag ) {
@@ -63,14 +66,10 @@ class DB {
 		}
 
 		exit( $encoded );
-
 	}	
 }
 
 $_db = new DB();
-$result = $_db->execute("SELECT*FROM user");
-
-
-print_r($result);
+$result = $_db->execute($_GET['sql']);
 	
 ?>
